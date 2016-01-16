afc730a8510a21c58d2a1092893eaf278b734351
David.Gao <david.alpha.fox@gmail.com>
2016-01-16 21:18:12 +0800

    修正ssl问题

--- tools/env_setup.sh
+++ tools/env_setup.sh
@@ -5,5 +5,5 @@ export CPLUS_INCLUDE_PATH
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/:/usr/local/opt/openssl/lib/
 export LD_LIBRARY_PATH
-LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
+LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib:/usr/local/opt/openssl/lib/
 export LIBRARY_PATH
