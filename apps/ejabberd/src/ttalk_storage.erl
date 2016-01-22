cc518c50dfde2c123c0037b7489c8843408207b0
David.Gao <david.alpha.fox@gmail.com>
2016-01-22 20:56:57 +0800

    增加存储模块

---
+++ apps/ejabberd/src/ttalk_storage.erl
@@ -0,0 +1,14 @@
+-module(ttalk_storage).
+-type backend() :: ttalk_storage_riak | ttalk_storage_redis.
+
+-spec storage_backend(backend()) -> string().
+storage_backend(Backend) ->
+    lists:flatten(
+      ["-module(ttalk_storage_backend).
+        -export([backend/0]).
+
+        -spec backend() -> atom().
+        backend() ->
+            ttalk_storage_",
+       atom_to_list(Backend),
+       ".\n"]).
